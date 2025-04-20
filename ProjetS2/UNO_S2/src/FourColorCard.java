17539b73dcd72561ef9e05c78965d4efa47cc65b
Hamidi Amine <155473689+Amine77zz@users.noreply.github.com>
2025-04-20 16:25:17 +0100

    Delete ProjetS2/UNO_S2 directory

--- ProjetS2/UNO_S2/src/FourColorCard.java
+++
@@ -1,7 +0,0 @@
-public class FourColorCard extends WildCard {
-    public String effect = "4color";
-
-    public FourColorCard() {
-        super();
-    }
-}
--- ProjetS2/UNO_S2/src/Z/ZButton.java
+++
@@ -1,8 +0,0 @@
-package Z;
-import javax.swing.JButton;
-
-public class ZButton extends JButton {
-public ZButton (String text) {
-	super(text) ;
-}
-}
